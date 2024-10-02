e9ec36509fab293735a9df86965673aa2e91abf4
Lunathor <mrcoopgmz@gmail.com>
2024-10-03 01:45:02 +0300

    Модель дальше вью

--- word_counter_project/word_counter/models.py
+++ word_counter_project/word_counter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class WordCount(models.Model):
+    word = models.CharField(max_length=255)
+    count = models.IntegerField(default=0)
+    
+    
